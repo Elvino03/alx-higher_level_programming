3b1414e67a6d9f18788766379ded5d544f5cafa3
Elvis Odinigwe <102396151+Elvino03@users.noreply.github.com>
2023-06-20 17:14:06 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping /0-readme.js
+++ 0x14-javascript-web_scraping /0-readme.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+
+function readFileContent(filePath) {
+  fs.readFile(filePath, 'utf-8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+// Usage: Provide the file path as the first argument when running the script
+const filePath = process.argv[2];
+readFileContent(filePath);

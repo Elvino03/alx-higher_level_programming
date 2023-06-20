319c4989230586a4b365a3ef3f0e271580b79cb1
Elvis Odinigwe <102396151+Elvino03@users.noreply.github.com>
2023-06-20 16:21:25 +0100

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # sends a request to a URL, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+if [ $# -eq 0 ]; then
+  echo "Error: URL argument missing."
+  exit 1
+fi
+
+URL="$1"
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
+
+echo "Status code: $STATUS_CODE"
